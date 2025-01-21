06442d954dab8213a0c9fda034f21329a0d7d6a9
lothergrabcad <lotheravanti@yahoo.com>
2025-01-21 21:13:35 +0200

    Input field test

--- SunblazeFE/SunblazeFE/UnitTest1.cs
+++ SunblazeFE/SunblazeFE/UnitTest1.cs
@@ -11,11 +11,21 @@ namespace SunblazeFE
         }
 
         [Test]
-        public void Test1()
+        public void OpenHomepage()
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com");
             Thread.Sleep(5000);
+            driver.Quit();
+        }
+
+        [Test]
+        public void InputNumber()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/inputs");
+            IWebElement webElement = driver.FindElement(By.XPath("//div/p[text()='Number']/following-sibling::input[1]"));
+            webElement.SendKeys("23");
             driver.Quit();
         }
     }

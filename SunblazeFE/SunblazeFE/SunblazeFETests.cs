cd6d27eda125f7b5deb5a562cf14fd36c51e23fe
lothergrabcad <lotheravanti@yahoo.com>
2025-01-23 16:44:22 +0200

    Added dropdown test

--- SunblazeFE/SunblazeFE/SunblazeFETests.cs
+++ SunblazeFE/SunblazeFE/SunblazeFETests.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace SunblazeFE
 {
@@ -26,6 +27,19 @@ namespace SunblazeFE
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/inputs");
             IWebElement webElement = driver.FindElement(By.XPath("//div/p[text()='Number']/following-sibling::input[1]"));
             webElement.SendKeys("23");
+            // Simplified form, no need to create variable
+            // driver.FindElement(By.XPath("//div/p[text()='Number']/following-sibling::input[1]")).SendKeys("23")
+            driver.Quit();
+        }
+
+        [Test]
+        public void SelectFromDropdown()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dropdown");
+            SelectElement selectElement = new SelectElement(driver.FindElement(By.Id("dropdown")));
+            selectElement.SelectByText("Option 2");
+            Thread.Sleep(5000);
             driver.Quit();
         }
     }

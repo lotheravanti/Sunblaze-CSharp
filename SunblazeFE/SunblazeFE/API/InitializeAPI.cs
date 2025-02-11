510d8528cf9ba20f21238dc2c27a3efc555f6f61
lothergrabcad <lotheravanti@yahoo.com>
2025-02-11 16:21:44 +0200

    Add GET API

--- SunblazeFE/SunblazeFE/API/InitializeAPI.cs
+++ SunblazeFE/SunblazeFE/API/InitializeAPI.cs
@@ -5,6 +5,22 @@ namespace SunblazeFE.API
 {
     public class InitializeAPI
     {
+        //Login
+        public object _requestBodyLogin => new
+        {
+            email = "eve.holt@reqres.in",
+            password = "cityslicka"
+        };
+        public string _urlLogin => "login";
+        //Create User
+        public object _requestBodyUsers => new
+        {
+            name = "lother",
+            job = "qa automation"
+        };
+        public string _urlUsers => "users";
+        //Get Users
+        public string _urlGetUsers => "users?page=2";
         public async Task<IAPIRequestContext> InitializeRequestContext()
         {
             var playwright = await Playwright.CreateAsync();
@@ -18,7 +34,20 @@ namespace SunblazeFE.API
         public async Task<JsonElement> PostAPI(string urlString, object requestBody)
         {
             var requestContext = await InitializeRequestContext();
+            //Send request using requestBody arg as body
             var response = await requestContext.PostAsync(url: urlString, new APIRequestContextOptions()
+            {
+                DataObject = requestBody
+            });
+            //Get Response
+            var responseJSON = await response.JsonAsync();
+            //Add JsonElement return type
+            return (JsonElement)responseJSON;
+        }
+        public async Task<JsonElement> GetAPI(string urlString, object? requestBody = null)
+        {
+            var requestContext = await InitializeRequestContext();
+            var response = await requestContext.GetAsync(url: urlString, new APIRequestContextOptions()
             {
                 DataObject = requestBody
             });
@@ -29,11 +58,31 @@ namespace SunblazeFE.API
         }
         public InitializeAPI() 
         {
+
         }
     }
 
     public class AuthenticateToken
     {
         public string? token { get; set; }
+    }
+
+    //Class acts as schema for JSON response
+    public class CreateUser
+    {
+        public string? name { get; set; }
+        public string? job { get; set; }
+        public string? id { get; set; }
+        public string? createdAt { get; set; }
+    }
+
+    public class GetUsers
+    {
+        public int? page { get; set; }
+        public int? per_page { get; set; }
+        public int? total { get; set; }
+        public int? total_pages { get; set; }
+        public Object[]? data {  get; set; }
+        public object? support { get; set; }
     }
 }
--- SunblazeFE/SunblazeFE/SunblazePlaywrightAPI.cs
+++ SunblazeFE/SunblazeFE/SunblazePlaywrightAPI.cs
@@ -6,24 +6,46 @@ namespace SunblazeFE
     //Inherit PageTest for Expect methods
     public class PlaywrightAPITests
     {
+        InitializeAPI initializeAPI;
         [SetUp]
         public void Setup()
         {
-            
+            InitializeAPI _initializeAPI = new();
+            initializeAPI = _initializeAPI;
         }
 
         [Test]
-        public async Task GetToken()
+        public async Task POSTGetLoginToken()
         {
-            object requestBody = new
-            {
-                email = "eve.holt@reqres.in",
-                password = "cityslicka"
-            };
-            InitializeAPI getResponse = new();
-            var requestResponse = await getResponse.PostAPI("login", requestBody);
+            var requestResponse = await initializeAPI.PostAPI(initializeAPI._urlLogin, initializeAPI._requestBodyLogin);
             var responseJSON = requestResponse.Deserialize<AuthenticateToken>();
             Console.WriteLine(responseJSON?.token);
+        }
+
+        [Test]
+        public async Task POSTCreateUser()
+        {
+            var requestResponse = await initializeAPI.PostAPI(initializeAPI._urlUsers, initializeAPI._requestBodyUsers);
+            //Deserialize JSON using Class as schema
+            var responseJSON = requestResponse.Deserialize<CreateUser>();
+            Console.WriteLine(responseJSON?.name);
+            Console.WriteLine(responseJSON?.job);
+            Console.WriteLine(responseJSON?.id);
+            Console.WriteLine(responseJSON?.createdAt);
+        }
+
+        [Test]
+        public async Task GETUsers()
+        {
+            var requestResponse = await initializeAPI.GetAPI(initializeAPI._urlGetUsers);
+            //Deserialize JSON using Class as schema
+            var responseJSON = requestResponse.Deserialize<GetUsers>();
+            Console.WriteLine(responseJSON?.page);
+            Console.WriteLine(responseJSON?.per_page);
+            Console.WriteLine(responseJSON?.total);
+            Console.WriteLine(responseJSON?.total_pages);
+            Array.ForEach(responseJSON?.data, Console.WriteLine);
+            Console.WriteLine(responseJSON?.support);
         }
 
         [TearDown]

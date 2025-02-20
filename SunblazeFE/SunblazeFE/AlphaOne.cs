369a37bebb1ba5cb5e50c45a85a5c1cb36f00dcf
lothergrabcad <lotheravanti@yahoo.com>
2025-02-20 19:25:07 +0200

    Update AlphaOne.cs

--- SunblazeFE/SunblazeFE/AlphaOne.cs
+++ SunblazeFE/SunblazeFE/AlphaOne.cs
@@ -5,27 +5,42 @@ namespace SunblazeFE
     public class AlphaOne
     {
         //Primitives
-        int integer = 10;
-        double decimalValue = 12.45;
-        float realNumber = 3.1417639f;
-        long largeNumber = 100000000000L;
+        static readonly int integer = 10;
+        static readonly double decimalValue = 12.45;
+        static readonly float realNumber = 3.1417639f;
+        static readonly long largeNumber = 100000000000L;
         //Max value for byte is 100
-        byte binaryValue = 100;
-        bool booleanValue = true;
+        static readonly byte binaryValue = 100;
+        static readonly bool booleanValue = true;
         //Single quotes for Character
-        char character = 'a';
+        static readonly char character = 'a';
 
         //Non Primitives
         //Double quotes for String
-        static String message = "Alpha One Initialized";
-        static String lowerCase = "lower case text";
-        String replacedMessage = message.Replace("One", "Prime");
+        static readonly String message = "Alpha One Initialized";
+        //Arrays
+        static readonly int[] integerArray = [1, 2, 3, 4, 5, 6, 7];
+        static readonly String[] stringArray = ["This", "is", "an", "Array", "from", "a", "String."];
+
+        //String Operations
+        static readonly String stringValue = "lower case text";
+        //Get length of String
         //Convert to Upper Case
-        String upperCase = lowerCase.ToUpper();
-        bool startsWith = message.StartsWith("Alpha");
-        bool endsWith = message.EndsWith("Initialized");
-        int[] integerArray = { 1, 1 };
-        String[] stringArray = { "First", "Second" };
+        static readonly String upperCase = stringValue.ToUpper();
+        //Reverse String
+        //Replace part of String
+        static readonly String replacedMessage = message.Replace("One", "Prime");
+        //Get last N characters of a String
+        //Remove First and Last characters of String
+        //Check if String is Alphabet
+        //Verify if String starts or ends with
+        static readonly bool startsWith = message.StartsWith("Alpha");
+        static readonly bool endsWith = message.EndsWith("Initialized");
+        //Check if String is Upper Case or Lower Case
+
+        //Array Operations
+        //Generate String from Array with delimiter
+        //Reverse Array
 
         [SetUp]
         public void Setup()
@@ -37,6 +52,7 @@ namespace SunblazeFE
         public void DisplayMessage()
         {
             Console.WriteLine(replacedMessage + " starts with Alpha: " + startsWith + " and ends with Initialized: " + endsWith);
+            Console.WriteLine(stringArray[0]);
         }
 
         //Add a wait at the end of every test for visibility when running manually

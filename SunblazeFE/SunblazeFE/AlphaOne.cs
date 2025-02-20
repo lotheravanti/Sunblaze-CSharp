c716d5a4772cdcef5c2f04ef3071db3fce5c559e
lothergrabcad <lotheravanti@yahoo.com>
2025-02-20 17:10:50 +0200

    Create AlphaOne.cs

---
+++ SunblazeFE/SunblazeFE/AlphaOne.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework.Internal;
+
+namespace SunblazeFE
+{
+    public class AlphaOne
+    {
+        //Primitives
+        int integer = 10;
+        double decimalValue = 12.45;
+        float realNumber = 3.1417639f;
+        long largeNumber = 100000000000L;
+        //Max value for byte is 100
+        byte binaryValue = 100;
+        bool booleanValue = true;
+        //Single quotes for Character
+        char character = 'a';
+
+        //Non Primitives
+        //Double quotes for String
+        static String message = "Alpha One Initialized";
+        static String lowerCase = "lower case text";
+        String replacedMessage = message.Replace("One", "Prime");
+        //Convert to Upper Case
+        String upperCase = lowerCase.ToUpper();
+        bool startsWith = message.StartsWith("Alpha");
+        bool endsWith = message.EndsWith("Initialized");
+        int[] integerArray = { 1, 1 };
+        String[] stringArray = { "First", "Second" };
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void DisplayMessage()
+        {
+            Console.WriteLine(replacedMessage + " starts with Alpha: " + startsWith + " and ends with Initialized: " + endsWith);
+        }
+
+        //Add a wait at the end of every test for visibility when running manually
+        //Ensure driver is closed
+        [TearDown]
+        public void Dispose()
+        {
+            
+        }
+    }
+}
